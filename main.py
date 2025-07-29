766109d456607369c109c4bf51817dc712f51a49
Manuel Garcia F. <mgarciafrigo@gmail.com>
2025-07-29 10:14:20 -0300

    cambiar valores iniciales

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ import random
 #
 
 tiempo_total = 2000  # ms
-tiempo_paso_simulacion = 200 # ms, tiempo para graficar cada ciclo
+tiempo_paso_simulacion = 500  # ms, tiempo para graficar cada ciclo
 
 # Configurables mediante casillas de texto
 params = {
-    "tam buffer rx": 16000,          # Bytes, determina el error superior
+    "tam buffer rx": 6400,           # Bytes, determina el error superior
     "nivel deseado (%)": 0.95,       # Valor nominal
     "mss": 536,                      # Tamaño máximo por ciclo
     "tiempo scan": 14,               # Tiempo de scan
     "consumo aplicacion": 128,       # Numero de bytes consumidos por ciclo
     "probabilidad perturbacion": 20, # Dada como porcentaje, un número entre 0 y 100
-    "limite bytes perturbados": 64,  # El número de bytes perdidos va entre 0 y este número
+    "limite bytes perturbados": 1024,  # El número de bytes perdidos va entre 0 y este número
 }
 
 
